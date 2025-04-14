2766e9b5fd864704a1c011864122adacc4bc0a8a
Renata-fernandez <rlfernandez@mail.austral.edu.ar>
2025-04-14 15:46:27 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,9 @@
 def leap_year():
-    print("TO DO")
+   year = int(input("Ingrese un año: "))
+	if year/4==year//4: 
+		if year/100 and year/400==year//400
+			print(f"El año {year} es bisiesto")
+		else: 
+			print(f"El año {year} no es bisiesto")
+	else: 
+		print(f"El año {year} no es bisiesto")
