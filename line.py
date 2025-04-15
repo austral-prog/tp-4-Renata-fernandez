a3050f9ceaf70d411cb2dfea627f94ad7c878c84
Renata-fernandez <rlfernandez@mail.austral.edu.ar>
2025-04-14 23:10:02 -0300

    Update line.py

--- line.py
+++ line.py
@@ -3,20 +3,20 @@ def line():
     B = float(input("Ingrese coeficiente B: "))
     X1 = float(input("Ingrese coeficiente X1: "))
     X2 = float (input("Ingrese coeficiente X2: "))
-    print(f"El coeficiente A de su ecuacion de la recta es: {A}")
-    print(f"El coeficiente B de su ecuacion de la recta es: {B}")
-    print(f"El coeficiente X1 su ecuacion de la recta es: {X1}")
-    print(f"El coeficiente X2 su ecuacion de la recta es: {X2}")
+    print(f"El coeficiente A de su ecuación de la recta es: {A}")
+    print(f"El coeficiente B de su ecuación de la recta es: {B}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {X2}")
     
     print("\nPara la siguiente ecuación:")
-    print(f"\tY= {A}X + {B}")
+    print(f"\tY = {A}X + {B}")
     
     Y1 = (A*X1)+B
     Y2 = (A*X2)+B
     P1 = f"({X1},{Y1})"
     P2 = f"({X2},{Y2})"
 
-    print(f"\n Dados los siguiente puntos:") 
+    print(f"\nDados los siguientes puntos:") 
     print(f"\tP1 {P1}")
     print(f"\tP2 {P2}")
     Distancia = (((X1-X2)**2) + ((Y1-Y2)**2))**(1/2) 
